2239bfe278aee35d0b37fac8b1375562c8167183
mr01robot <42098976+mr01robot@users.noreply.github.com>
2022-01-12 18:42:44 -0500

    detalles

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,17 +1,15 @@
-// eslint-disable-next-line no-unused-vars
 import React, {useState, useEffect} from "react";
 
-// eslint-disable-next-line no-unused-vars
 const Product = ({objetProduct, setContentProd}) => {
 	const getRandomArbitrary = (a, b) => {
 		const num = Math.random() * (b - a) + a;
 		return new Date(num * 1000 * 60);
 	};
-	// eslint-disable-next-line no-unused-vars
+
 	const [minu, setMinu] = useState(getRandomArbitrary(1, 3).getTime());
 
-	// let temporizador;
-	/*	useEffect(() => {
+	let temporizador;
+	useEffect(() => {
 		if (minu >= 1000) {
 			let minutos = minu;
 			temporizador = setInterval(() => {
@@ -20,7 +18,7 @@ const Product = ({objetProduct, setContentProd}) => {
 			}, 1000);
 		}
 		return () => clearInterval(temporizador);
-	}, [minu]); */
+	}, [minu]);
 	return (
 		<div className="card">
 			<img src={objetProduct.image} alt="" className="card__img" />
